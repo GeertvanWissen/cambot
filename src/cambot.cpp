1e036f5c40d6e2d2abce7919fb750e0cd7e52a85
geertvanwissen <geert@delete.nl>
2016-09-17 12:42:35 +0200

    werkt!

--- src/cambot.cpp
+++ src/cambot.cpp
@@ -13,17 +13,17 @@ int main (void)
     string inputstate;
     
     // create objects for I/O
-    GPIOClass* motor1A = new GPIOClass("16");
-    GPIOClass* motor1B = new GPIOClass("18");
-    GPIOClass* motor1E = new GPIOClass("22");
+    GPIOClass* motor1A = new GPIOClass("23");
+    GPIOClass* motor1B = new GPIOClass("24");
+    GPIOClass* motor1E = new GPIOClass("25");
     
-    GPIOClass* sw_frontL = new GPIOClass("40");
-    GPIOClass* sw_frontR = new GPIOClass("38");
+    GPIOClass* sw_frontL = new GPIOClass("21");
+    GPIOClass* sw_frontR = new GPIOClass("20");
 
     // export GPIOs to be able to read and write
     motor1A->export_gpio();
-    motor1A->export_gpio();
-    motor1A->export_gpio();
+    motor1B->export_gpio();
+    motor1E->export_gpio();
     
     sw_frontL->export_gpio();
     sw_frontR->export_gpio();
@@ -40,35 +40,26 @@ int main (void)
 
     cout << " Set GPIO pin directions" << endl;
 
+    motor1A->setval_gpio("1");
+    motor1B->setval_gpio("0");
+
+    cout << "motor 1 set" << endl;
+
     while(1)
     {
         // just some code to get a sense of writing in C
         
         sw_frontL->getval_gpio(inputstate); //read state of input example
-        cout << "Current input pin state is " << inputstate  <<endl;
-        if(inputstate == "0") // if input pin is at state "0" i.e. button pressed
-        {
-            cout << "input pin state is Pressed. Will check input pin state again in 20ms "<<endl;
-                usleep(20000);
-                    cout << "Checking again ....." << endl;
-                    sw_frontL->getval_gpio(inputstate); // checking again to ensure that state "0" is due to button press and not noise
-            if(inputstate == "0")
-            {
-                cout << "input pin state is definitely Pressed. Turning LED ON" <<endl;
-                motor1E->setval_gpio("1"); // turn motor ON
+        cout << "Front left switch state is " << inputstate  <<endl;
+	
+	if (inputstate == "1") {
+		        motor1E->setval_gpio("1");
+			cout << "motor should be on" << endl;
+	}else{
+			motor1E->setval_gpio("0");
 
-                cout << " Waiting until pin is unpressed....." << endl;
-                while (inputstate == "0"){
-                sw_frontL->getval_gpio(inputstate);
-                };
-                cout << "pin is unpressed" << endl;
-
-            }
-            else
-                cout << "input pin state is definitely UnPressed. That was just noise." <<endl;
-
-        }
-        motor1E->setval_gpio("0");
+	}
+	usleep(20000);
 
     }
     cout << "Exiting....." << endl;
