534ee831b7867874957c3bb0808ce55956ff515c
geertvanwissen <geert@delete.nl>
2016-09-17 16:38:30 +0200

    blaat

--- src/cambot.cpp
+++ src/cambot.cpp
@@ -4,46 +4,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "GPIOClass.h"
+#include "move.h"
 
 using namespace std;
 
 int main (void)
 {
+    Move::init();
 
     string inputstate;
-    
     // create objects for I/O
-    GPIOClass* motor1A = new GPIOClass("23");
-    GPIOClass* motor1B = new GPIOClass("24");
-    GPIOClass* motor1E = new GPIOClass("25");
-    
     GPIOClass* sw_frontL = new GPIOClass("21");
     GPIOClass* sw_frontR = new GPIOClass("20");
 
     // export GPIOs to be able to read and write
-    motor1A->export_gpio();
-    motor1B->export_gpio();
-    motor1E->export_gpio();
-    
     sw_frontL->export_gpio();
     sw_frontR->export_gpio();
 
-    cout << " GPIO pins exported" << endl;
-
     // set direction for GPIOs
-    motor1A->setdir_gpio("out");
-    motor1B->setdir_gpio("out");
-    motor1E->setdir_gpio("out");
-
     sw_frontL->setdir_gpio("in");
     sw_frontR->setdir_gpio("in");
-
-    cout << " Set GPIO pin directions" << endl;
-
-    motor1A->setval_gpio("1");
-    motor1B->setval_gpio("0");
-
-    cout << "motor 1 set" << endl;
 
     while(1)
     {
@@ -53,10 +33,10 @@ int main (void)
         cout << "Front left switch state is " << inputstate  <<endl;
 	
 	if (inputstate == "1") {
-		        motor1E->setval_gpio("1");
+		        Move::direction(2);
 			cout << "motor should be on" << endl;
 	}else{
-			motor1E->setval_gpio("0");
+			Move::stop();
 
 	}
 	usleep(20000);
