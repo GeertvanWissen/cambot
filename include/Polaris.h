1cb650a6550cdba0d536b74b21bade922f0a0897
GeertvanWissen <geert@delete.nl>
2016-09-18 12:17:08 +0200

    Update Polaris.h

--- include/Polaris.h
+++ include/Polaris.h
@@ -4,31 +4,18 @@
 class Polaris {
 
 public:
-  CoordsList getAllCoords // get a list of all coordinates
-  bool save(string name) // save the current position as 'name' to compare to later
-  Diff compare(string savedcoords) // compare current position to saved coordinated
+  struct Coords
+  {
+    int x;
+    int y;
+  };
   
+  vector<Coords> coordslist;
+  
+  Polaris()
+  {
+    // constructor
+  }
 }
-
-class Coords {
-public:
-  int x;
-  int y;
-  string name;
-}
-
-class SavedCoords:Coords {
-
-}
-
-class CoordsList {
-
-}
-
-class Diff {
-
-}
-
-
 
 #endif
