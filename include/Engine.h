7527e19fa8022a5aeb95f6f6193cac588c8a2813
GeertvanWissen <geert@delete.nl>
2016-09-18 10:03:23 +0200

    Update Engine.h

--- include/Engine.h
+++ include/Engine.h
@@ -1,7 +1,17 @@
+#include "GPIOClass.h"
+
 class Engine {
 private:
   int status;
+  GPIOClass* motor1A;
+  GPIOClass* motor1B;
+  GPIOClass* motor1E;
+  GPIOClass* motor2A;
+  GPIOClass* motor2B;
+  GPIOClass* motor2E;
+  
 public:
+  Engine();
   bool (int dir);
   bool stop();
   int getStatus
