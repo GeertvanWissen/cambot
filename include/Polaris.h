28cc0b477538b50319a917987519eb60b68055e6
GeertvanWissen <geert@delete.nl>
2016-09-18 10:38:58 +0200

    Create Polaris.h

---
+++ include/Polaris.h
@@ -0,0 +1,29 @@
+// object to retrieve coordinates to navigate with
+
+class Polaris {
+
+public:
+  CoordsList getAllCoords // get a list of all coordinates
+  bool save(string name) // save the current position as 'name' to compare to later
+  Diff compare(string savedcoords) // compare current position to saved coordinated
+  
+}
+
+class Coords {
+public:
+  int x;
+  int y;
+  string name;
+}
+
+class SavedCoords:Coords {
+
+}
+
+class CoordsList {
+
+}
+
+class Diff {
+
+}
