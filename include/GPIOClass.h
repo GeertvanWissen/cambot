da5f64be929e69f3bf2b5adba1c4222c1b0e595f
GeertvanWissen <geert@delete.nl>
2016-09-18 10:48:07 +0200

    Update GPIOClass.h

--- include/GPIOClass.h
+++ include/GPIOClass.h
@@ -15,8 +15,8 @@ public:
     int export_gpio(); // exports GPIO
     int unexport_gpio(); // unexport GPIO
     int setdir_gpio(string dir); // Set GPIO Direction
-    int setval_gpio(string val); // Set GPIO Value (putput pins)
-    int getval_gpio(string& val); // Get GPIO Value (input/ output pins)
+    int setval_gpio(string val); // Set GPIO Value of output pins
+    int getval_gpio(string& val); // Get GPIO Value (both input/output pins)
     string get_gpionum(); // return the GPIO number associated with the instance of an object
 private:
     string gpionum; // GPIO number associated with the instance of an object
